ccf9786af02980a98de07220f6f93039096a79de
vdavids <tdavids@hopjump.com>
2020-04-18 17:09:40 -0400

    NETOWRK LOL

--- metrics.py
+++ metrics.py
@@ -3,12 +3,12 @@ from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
 registry = CollectorRegistry()
 download_bandwidth_gauge = Gauge('download_bandwidth', 'download bandwidth', ['network'], registry=registry)
 download_bytes_gauge = Gauge('download_speed', 'download speed (bits per second)', ['network'], registry=registry)
-upload_bandwidth_gauge = Gauge('upload_bandwidth', 'upload bandwidth', ['netowrk'], registry=registry)
+upload_bandwidth_gauge = Gauge('upload_bandwidth', 'upload bandwidth', ['network'], registry=registry)
 upload_bytes_gauge = Gauge('upload_speed', 'upload speed (bits per second)', ['network'], registry=registry)
 ping_gauge = Gauge('ping_latency', 'download speed (milliseconds)', ['network'], registry=registry)
 
 def record(network, test):
-    download_bandwidth_gauge.labels(netowrk=network).set(test.download_bandwidth)
+    download_bandwidth_gauge.labels(network=network).set(test.download_bandwidth)
     download_bytes_gauge.labels(network=network).set(test.download_bytes)
     upload_bandwidth_gauge.labels(network=network).set(test.upload_bandwidth)
     upload_bytes_gauge.labels(network=network).set(test.upload_bytes)
