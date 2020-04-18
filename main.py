7b82b02f776750099531304863f5e93c7da46182
MisterSquishy <pdavids@comcast.net>
2020-04-18 09:09:32 -0400

    add argparse for easier testing

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ from speedtest import SpeedTest
 import wifi
 
 def test(network):
-    wifi.connect(network)
-    print(f"testing speed on {wifi.get_current_network_name()}")
-    test = SpeedTest()
-    test.print_results() # todo aggregate and display these someplace
+    try:
+        if(not wifi.get_current_network_name() == network):
+            print(f"attempting to connect to {network}")
+            wifi.connect(network)
+        print(f"testing speed on {wifi.get_current_network_name()}")
+        test = SpeedTest()
+        test.print_results() # todo aggregate and display these someplace
+    except wifi.ConnectionException as e:
+        print(e)
+
+def test_all():
+    [test(network) for network in wifi.get_all_possible_networks()]
 
 if __name__ == "__main__":
-    for network in wifi.get_all_possible_networks():
-        print(f"attempting to connect to {network}")
-        try:
-            test(network)
-        except wifi.ConnectionException as e:
-            print(e)
+    import argparse
+    parser = argparse.ArgumentParser(description='test the network speed')
+    parser.add_argument('--network', type=str, default="", help='name of network to test (optional)')
+    args = parser.parse_args()
+    if(args.network):
+        test(args.network)
+    else:
+        test_all()
